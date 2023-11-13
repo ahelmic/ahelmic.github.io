757a095dd9761da9e7e7adfc0c03e6040614a2b3
ahelmic <helmickaustin312@gmail.com>
2023-11-13 09:24:20 -0500

    My first commit

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+/*
+	Student Name: Austin Helmick
+	File Name: script.js
+	Date: 11/9/2023
+*/
+
+// Hamburger menu function
+// Hamburger menu function
+function hamburger() {
+    var mobileNav = document.querySelector(".mobile-nav");
+    mobileNav.classList.toggle("show-menu");
+}
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
+
+
+	
+	
